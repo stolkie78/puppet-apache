ce9b918a7623745d985c1569405ac1a9125c6ca1
Giedo Stolk <stolkie@gmail.com>
2020-11-11 16:42:30 +0100

    Stuff

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,12 +2,9 @@
 #
 # A description of what this class does
 #
-class apache::install (
-  $install_ensure = $apache::params::install_ensure,
-  $install_name   = $apache::params::install_name
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => "${install_ensure}",
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => "${apache::install_ensure}",
   }
 }
 
