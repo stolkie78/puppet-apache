3536d26fd87ca959fe63d3eff52fec6f01cb1096
Giedo Stolk <giedo@gsit.nl>
2020-11-11 16:11:24 +0100

    ja ja

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,12 @@
 #
 # A description of what this class does
 #
-class apache::install {
-  package { 'httpd':
-    ensure => 'present',
+class apache::install (
+  $install_ensure = $apache::params::install_ensure,
+  $install_name   = $apache::params::install_name
+) inherits apache::params {
+  package { "${install_name}":
+    ensure => "${install_ensure}",
   }
 }
 
---
+++ manifests/params.pp
@@ -0,0 +1,19 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include apache::params
+class apache::params {
+  $install_ensure = 'present'
+
+  case $facts['os']['family'] {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+    default: {}
+  }
+}
